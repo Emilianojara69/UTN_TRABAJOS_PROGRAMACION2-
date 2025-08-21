d9dd909cabf1e76a2d192b9e2c040015c9161a50
Emiliano jara <96025262+Emilianojara69@users.noreply.github.com>
2025-08-21 00:36:57 -0300

    Add files via upload

---
+++ TRABAJO PRACTICO 1/PROGRAMA/src/tp1/programacion/pkg2/TP.java
@@ -0,0 +1,132 @@
+
+package tp1.programacion.pkg2;
+
+/**
+ * @author EMILIANO_DEV
+ */
+
+/*public class HolaMundo {
+    public static void main(String[] args){
+        System.out.println("¡Hola, Java!");
+    }
+}*/
+
+/*public class Variables {
+    public static void main(String[] args){
+        String nombre= "Emiliano";
+        int edad = 24;
+        double altura = 1.72;
+        boolean estudiante = true;
+
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println("Altura: " + altura);
+        System.out.println("¿Es estudiante?: " + estudiante);
+
+
+   
+    }
+} 
+**
+*
+*/
+
+import java.util.Scanner;
+
+/*public class DatosUsuario{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Ingresa tu nombre: ");
+        String nombre = sc.nextLine();
+        
+        System.out.print("ingresa tu edad: ");
+        int edad = sc.nextInt();
+        
+        System.out.println("Tu nombre es " + nombre + "y tenes " + edad + " años" );
+    }
+}
+*/
+
+/* public class Operaciones {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingresa el primer numero: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Ingresa el segundo numero: ");
+        int num2 = sc.nextInt();
+
+        System.out.println("Suma: " + (num1 + num2));
+        System.out.println("Resta: " + (num1 - num2));
+        System.out.println("Multiplicacion: " + (num1 * num2));
+        System.out.println("Division: " + (num1 / num2));
+    }
+}
+*/
+
+/*public class Mensaje {
+    public static void main(String[] args) {
+        System.out.println("Nombre: Juan Pérez\nEdad: 30 años\nDirección: \"Calle Falsa 123\"");
+    }
+}*/
+
+/*public class Expresiones{
+   public static void main(String[] args){
+        int x = 10;     
+        x = x + 5;        
+        System.out.println(x);
+   } 
+}*/
+
+
+/* public class DivisionEnteros {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingresa el primer número entero: ");
+        int a = sc.nextInt();
+
+        System.out.print("Ingresa el segundo número entero: ");
+        int b = sc.nextInt();
+
+        System.out.println("Resultado: " + (a / b));
+    }
+}*/
+
+/*
+public class DivisionDoubles {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingresa el primer número: ");
+        double a = sc.nextDouble();
+
+        System.out.print("Ingresa el segundo número: ");
+        double b = sc.nextDouble();
+
+        System.out.println("Resultado: " + (a / b));
+    }
+}
+*/
+
+/*
+public class ErrorEjemplo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingresa tu nombre: ");
+        String nombre = scanner.nextLine(); // CORREGIDO
+        System.out.println("Hola, " + nombre);
+    }
+}
+*/
+/*public class PruebaEscritorio {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 2;
+        int resultado = a / b;
+        System.out.println("Resultado: " + resultado);
+    }
+}
+*/
